504c3da2ea2ca9e5b1f8eb3f84f87455cf9bc487
suji an <tnwl_1014@daum.net>
2023-07-29 23:17:55 +0900

    [0729 write.java modify]

--- OurHome/src/main/java/action/QNAWriteFormAction.java
+++ OurHome/src/main/java/action/QNAWriteFormAction.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 public class QNAWriteFormAction implements CommandAction {
 
+
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
 		// TODO Auto-generated method stub
